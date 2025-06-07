22460222f49c5037c978417e35f62c68c5790b91
Rajeev <rajeevkumarmajhi@gmail.com>
2025-06-07 21:01:28 +0545

    Mortgage Payment Calculator using tutorial method

--- src/Main.java
+++ src/Main.java
@@ -13,21 +13,45 @@ public class Main {
     // M = P((r (1 + r) ^ n)/((1+r)^n - 1))
 
     public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter Principal (p) :" );
+//        int p = scanner.nextInt();
+//
+//        System.out.print("Enter rate (r) :" );
+//        float r = scanner.nextFloat();
+//
+//        System.out.print("Enter number of payments (n) :" );
+//        byte n = scanner.nextByte();
+//
+//        double numerator = r * Math.pow(1 + r, n);
+//        double denominator = Math.pow(1 + r, n) - 1;
+//        double m = p * (numerator / denominator);
+//
+//        System.out.printf("Monthly Payment (M): %.2f%n", m);
+
+//        Tutorial Solution
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter Principal (p) :" );
-        int p = scanner.nextInt();
+        System.out.print("Enter Principal :" );
+        int principal = scanner.nextInt();
 
-        System.out.print("Enter rate (r) :" );
-        float r = scanner.nextFloat();
+        System.out.print("Enter Annual Interest Rate :" );
+        float annualInterestRate = scanner.nextFloat();
+        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
 
-        System.out.print("Enter number of payments (n) :" );
-        byte n = scanner.nextByte();
+        System.out.print("Enter Period Years:" );
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
 
-        double numerator = r * Math.pow(1 + r, n);
-        double denominator = Math.pow(1 + r, n) - 1;
-        double m = p * (numerator / denominator);
+        double mortgage = principal * ( monthlyInterestRate * Math.pow( 1 + monthlyInterestRate , numberOfPayments ) )
+                            / ( Math.pow( 1 + monthlyInterestRate , numberOfPayments ) - 1 );
 
-        System.out.printf("Monthly Payment (M): %.2f%n", m);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage :" + mortgageFormatted);
+
     }
 }
