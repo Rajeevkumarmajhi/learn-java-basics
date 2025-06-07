3b34bf289f36205f3b6fc5448620657c562fa061
Rajeev <rajeevkumarmajhi@gmail.com>
2025-06-07 20:40:09 +0545

    Mortgage Payment Calculator using it's formulae

--- src/Main.java
+++ src/Main.java
@@ -9,128 +9,25 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        // Primitive Data
-//        byte age = 33;
-//        int count = 2000;
-//        long viewsCount = 3_123_456_789L;
-//        float price = 10.9F;
-//        char letter = 'A';
-//        boolean isEligible = false;
+    //    Creating Mortgage Payment Calculator
+    // M = P((r (1 + r) ^ n)/((1+r)^n - 1))
 
-        // Reference Types (Objects)
-//        Integer housesCount = 200000;
-//        String DeveloperName = "Rajeev Kumar Majhi";
-//        Date now = new Date();
-//
-//        Point point = new Point(5,6);
-//        Point point1 = point;
-//
-//        point.x = 9;
-//        System.out.println(point1.x);
-
-
-        // Arrays
-//        int[] numbers = { 2,4,6,3,5 };
-
-        // sort array
-//        Arrays.sort(numbers);
-//        System.out.println(Arrays.toString(numbers));
-
-        // Multi dimentional array
-//        int[][] numbers = new int[2][3];
-//        numbers[0][0] = 1;
-//        int [][] numbers = { {1,2,3}, {4,5,6} };
-//
-//
-//        System.out.println(Arrays.deepToString(numbers));
-
-//        Constants Declarations
-//        final float PI = 3.14F;
-//
-//        System.out.println(PI);
-
-//        Arithmetic Expression
-//        int x = 1;
-//        int y = x++;
-//        System.out.println(x);
-//        System.out.println(y);
-//
-//        print 2,1 since y= x first then x++
-
-//        int x = 1;
-//        int y = ++x;
-//        System.out.println(x);
-//        System.out.println(y);
-
-//      prints 2,2 since x is incremented first and then set to y
-
-//        int x = 1;
-//        x += 2;  // Augmented operator
-//
-//        System.out.println(x);
-
-//        Casting
-//        Implicit Casting (Casting automatically short to int in below example)
-//        byte > short > int > long > float > double
-//        short x = 1;
-//        int y = x + 2;
-//        System.out.println(y);
-
-//        Explicit Casting
-//        double num = 10.99;
-//        int result = (int) num;  // double to int (narrowing)
-//        System.out.println(result); // Output: 10
-
-
-//        Explicit Convertion using a method
-//        String str = "123";
-//        int num = Integer.parseInt(str);  // ✅ explicit conversion using a method
-//        System.out.println(num);
-
-//        The Math Class
-
-//        int result = Math.round(1.45F);
-//        System.out.println(result);
-
-//        int result = (int)Math.ceil(1.1F);
-//        int result2 = (int)Math.floor(1.1F);
-//        int result3 = Math.max(result,result2);
-//        int result4 = Math.min(result,result2);
-//        System.out.println(result);
-//        System.out.println(result2);
-//        System.out.println(result3);
-//        System.out.println(result4);
-
-//        Random Number
-//        int randomNumber = (int)(Math.random() * 100);
-//        System.out.println(randomNumber);
-
-//        Formatting Number
-//        Formatting Currency
-//        NumberFormat currency = NumberFormat.getCurrencyInstance();
-//        String result = currency.format(1234567.891);
-//        System.out.println(result);
-
-//        Formatting to percentage using method chaining
-//        String result = NumberFormat.getPercentInstance().format(0.1);
-//        System.out.println(result);
-
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Age : ");
-//        byte age = scanner.nextByte();
-//        System.out.println("You are " + age);
-
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Name : ");
-        String name = scanner.nextLine().trim();
-        System.out.println("You are " + name);
 
+        System.out.print("Enter Principal (p) :" );
+        int p = scanner.nextInt();
 
+        System.out.print("Enter rate (r) :" );
+        float r = scanner.nextFloat();
 
+        System.out.print("Enter number of payments (n) :" );
+        byte n = scanner.nextByte();
 
+        double numerator = r * Math.pow(1 + r, n);
+        double denominator = Math.pow(1 + r, n) - 1;
+        double m = p * (numerator / denominator);
 
-
-
+        System.out.printf("Monthly Payment (M): %.2f%n", m);
     }
 }
---
+++ src/MainBackup.java
@@ -0,0 +1,136 @@
+import java.awt.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MainBackup {
+    public static void main(String[] args) {
+        // Primitive Data
+//        byte age = 33;
+//        int count = 2000;
+//        long viewsCount = 3_123_456_789L;
+//        float price = 10.9F;
+//        char letter = 'A';
+//        boolean isEligible = false;
+
+        // Reference Types (Objects)
+//        Integer housesCount = 200000;
+//        String DeveloperName = "Rajeev Kumar Majhi";
+//        Date now = new Date();
+//
+//        Point point = new Point(5,6);
+//        Point point1 = point;
+//
+//        point.x = 9;
+//        System.out.println(point1.x);
+
+
+        // Arrays
+//        int[] numbers = { 2,4,6,3,5 };
+
+        // sort array
+//        Arrays.sort(numbers);
+//        System.out.println(Arrays.toString(numbers));
+
+        // Multi dimentional array
+//        int[][] numbers = new int[2][3];
+//        numbers[0][0] = 1;
+//        int [][] numbers = { {1,2,3}, {4,5,6} };
+//
+//
+//        System.out.println(Arrays.deepToString(numbers));
+
+//        Constants Declarations
+//        final float PI = 3.14F;
+//
+//        System.out.println(PI);
+
+//        Arithmetic Expression
+//        int x = 1;
+//        int y = x++;
+//        System.out.println(x);
+//        System.out.println(y);
+//
+//        print 2,1 since y= x first then x++
+
+//        int x = 1;
+//        int y = ++x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+//      prints 2,2 since x is incremented first and then set to y
+
+//        int x = 1;
+//        x += 2;  // Augmented operator
+//
+//        System.out.println(x);
+
+//        Casting
+//        Implicit Casting (Casting automatically short to int in below example)
+//        byte > short > int > long > float > double
+//        short x = 1;
+//        int y = x + 2;
+//        System.out.println(y);
+
+//        Explicit Casting
+//        double num = 10.99;
+//        int result = (int) num;  // double to int (narrowing)
+//        System.out.println(result); // Output: 10
+
+
+//        Explicit Convertion using a method
+//        String str = "123";
+//        int num = Integer.parseInt(str);  // ✅ explicit conversion using a method
+//        System.out.println(num);
+
+//        The Math Class
+
+//        int result = Math.round(1.45F);
+//        System.out.println(result);
+
+//        int result = (int)Math.ceil(1.1F);
+//        int result2 = (int)Math.floor(1.1F);
+//        int result3 = Math.max(result,result2);
+//        int result4 = Math.min(result,result2);
+//        System.out.println(result);
+//        System.out.println(result2);
+//        System.out.println(result3);
+//        System.out.println(result4);
+
+//        Random Number
+//        int randomNumber = (int)(Math.random() * 100);
+//        System.out.println(randomNumber);
+
+//        Formatting Number
+//        Formatting Currency
+//        NumberFormat currency = NumberFormat.getCurrencyInstance();
+//        String result = currency.format(1234567.891);
+//        System.out.println(result);
+
+//        Formatting to percentage using method chaining
+//        String result = NumberFormat.getPercentInstance().format(0.1);
+//        System.out.println(result);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Age : ");
+//        byte age = scanner.nextByte();
+//        System.out.println("You are " + age);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name : ");
+        String name = scanner.nextLine().trim();
+        System.out.println("You are " + name);
+
+
+
+
+
+
+
+    }
+}
