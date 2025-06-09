01f8eb62edc0136f8fe2a542fb19761693e65617
Rajeev <rajeevkumarmajhi@gmail.com>
2025-06-09 07:48:15 +0545

    Fizz Buzz mini app

--- src/Main.java
+++ src/Main.java
@@ -38,18 +38,33 @@ public class Main {
 //        System.out.println( (income > 200_000) ? "High Income" : "Low Income" );
 
 //        Switch Statement
-        String role = "admin";
+//        String role = "admin";
+//
+//        switch (role) {
+//            case "admin":
+//                System.out.println("You are admin");
+//                break;
+//            case "staff":
+//                System.out.println("You are staff");
+//            default:
+//                System.out.println("You are a guest");
+//        }
 
-        switch (role) {
-            case "admin":
-                System.out.println("You are admin");
-                break;
-            case "staff":
-                System.out.println("You are staff");
-            default:
-                System.out.println("You are a guest");
-        }
+//        Fizz Buzz mini app test
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number : ");
+
+        int number = scanner.nextInt();
+
+        if(number % 5 == 0 && number % 3 == 0)
+            System.out.println("FizzBuzz");
+        else if(number % 5 == 0)
+            System.out.println("Fizz");
+        else if(number % 3 == 0)
+            System.out.println("Buzz");
+        else
+            System.out.println(number);
 
 
 
