bc39ccca03f373c495292dde215820bfb93a265d
Rajeev <rajeevkumarmajhi@gmail.com>
2025-06-09 07:24:11 +0545

    Conditional Operators

--- src/Main.java
+++ src/Main.java
@@ -9,49 +9,24 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    //    Creating Mortgage Payment Calculator
-    // M = P((r (1 + r) ^ n)/((1+r)^n - 1))
-
     public static void main(String[] args){
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print("Enter Principal (p) :" );
-//        int p = scanner.nextInt();
-//
-//        System.out.print("Enter rate (r) :" );
-//        float r = scanner.nextFloat();
-//
-//        System.out.print("Enter number of payments (n) :" );
-//        byte n = scanner.nextByte();
-//
-//        double numerator = r * Math.pow(1 + r, n);
-//        double denominator = Math.pow(1 + r, n) - 1;
-//        double m = p * (numerator / denominator);
-//
-//        System.out.printf("Monthly Payment (M): %.2f%n", m);
+        int x = 1;
+        int y = 1;
 
-//        Tutorial Solution
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x <= y);
+        System.out.println(x >= y);
+        System.out.println(x < y);
+        System.out.println(y < x);
+        System.out.println(x > y);
+        System.out.println(y > x);
 
-        Scanner scanner = new Scanner(System.in);
+//        && AND || OR || ! Not
 
-        System.out.print("Enter Principal :" );
-        int principal = scanner.nextInt();
 
-        System.out.print("Enter Annual Interest Rate :" );
-        float annualInterestRate = scanner.nextFloat();
-        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
 
-        System.out.print("Enter Period Years:" );
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
 
-        double mortgage = principal * ( monthlyInterestRate * Math.pow( 1 + monthlyInterestRate , numberOfPayments ) )
-                            / ( Math.pow( 1 + monthlyInterestRate , numberOfPayments ) - 1 );
-
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage :" + mortgageFormatted);
 
     }
 }
---
+++ src/Mortgage.java
@@ -0,0 +1,57 @@
+import java.awt.*;
+        import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Mortgage {
+    //    Creating Mortgage Payment Calculator
+    // M = P((r (1 + r) ^ n)/((1+r)^n - 1))
+
+    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter Principal (p) :" );
+//        int p = scanner.nextInt();
+//
+//        System.out.print("Enter rate (r) :" );
+//        float r = scanner.nextFloat();
+//
+//        System.out.print("Enter number of payments (n) :" );
+//        byte n = scanner.nextByte();
+//
+//        double numerator = r * Math.pow(1 + r, n);
+//        double denominator = Math.pow(1 + r, n) - 1;
+//        double m = p * (numerator / denominator);
+//
+//        System.out.printf("Monthly Payment (M): %.2f%n", m);
+
+//        Tutorial Solution
+//        final byte MONTHS_IN_YEAR = 12;
+//        final byte PERCENT = 100;
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter Principal :" );
+//        int principal = scanner.nextInt();
+//
+//        System.out.print("Enter Annual Interest Rate :" );
+//        float annualInterestRate = scanner.nextFloat();
+//        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+//
+//        System.out.print("Enter Period Years:" );
+//        byte years = scanner.nextByte();
+//        int numberOfPayments = years * MONTHS_IN_YEAR;
+//
+//        double mortgage = principal * ( monthlyInterestRate * Math.pow( 1 + monthlyInterestRate , numberOfPayments ) )
+//                            / ( Math.pow( 1 + monthlyInterestRate , numberOfPayments ) - 1 );
+//
+//        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+//        System.out.println("Mortgage :" + mortgageFormatted);
+
+    }
+}
