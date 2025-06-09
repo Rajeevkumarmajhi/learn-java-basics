b7d67e0598654e69f816432c07ce35798585af35
Rajeev <rajeevkumarmajhi@gmail.com>
2025-06-09 07:52:27 +0545

    while Loop

--- src/Main.java
+++ src/Main.java
@@ -68,8 +68,13 @@ public class Main {
 
 //        For Loops
 
-        for (int  i = 0; i <= 5 ; i++)
-            System.out.println("Number : " + i);
+//        for (int  i = 0; i <= 5 ; i++)
+//            System.out.println("Number : " + i);
 
+        int i = 0;
+        while(i <= 5){
+            System.out.println("Number :" + i);
+            i++;
+        }
     }
 }
